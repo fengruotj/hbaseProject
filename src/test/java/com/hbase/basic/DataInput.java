70455b2b1bc4869920e3787474692e2f4ac474b1
谭杰 <798750509@qq.com>
2016-05-23 17:00:26 +0800

    2016 5 23 V1

--- src/test/java/com/hbase/basic/DataInput.java
+++ src/test/java/com/hbase/basic/DataInput.java
@@ -21,22 +21,22 @@ public class DataInput {
      * @throws IOException
      */
     @Test
-    public void addA06_OILFIELDINFO() throws IOException {
+    public void addOILFIELDINFO() throws IOException {
         Random random=new Random();
-        HBaseUtils.creatTable("A06_OILFIELDINFO",new String[]{"info"});
+        HBaseUtils.creatTable("OILFIELDINFO",new String[]{"info"});
         for(int i=1;i<=9;i++)
             for(int j=1;j<=9;j++){
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","YQTMC","油气田"+i);
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","YQTBM","A"+i);
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","PDMC","油气盆地"+i);
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","SSPDBM","P"+i);
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","NF",String.valueOf(2006+j));
-                HBaseUtils.addRecord("A06_OILFIELDINFO","A"+i,"info","MJ",String.valueOf(random.nextDouble()*300)+200);
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","YQTMC","油气田"+i);
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","YQTBM","A"+i);
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","PDMC","油气盆地"+i);
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","SSPDBM","P"+i);
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","NF",String.valueOf(2006+j));
+                HBaseUtils.addRecord("OILFIELDINFO","A"+i,"info","MJ",String.valueOf(random.nextDouble()*300)+200);
             }
     }
 
     @Test
-    public void deleteA06_OILFIELDINFO() throws IOException {
-        HBaseUtils.deleteTable("A06_OILFIELDINFO");
+    public void deleteOILFIELDINFO() throws IOException {
+        HBaseUtils.deleteTable("OILFIELDINFO");
     }
 }
